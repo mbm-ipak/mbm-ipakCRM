fc92f90c6f97511c18186a13e79cc2a48a366fb4
ieskandari <83347005+ieskandari@users.noreply.github.com>
2021-11-15 16:01:41 +0330

    یلق

--- include/models/TaskCRM.php
+++ include/models/TaskCRM.php
@@ -2,7 +2,7 @@
 $this->models["TaskCRM"] = [];
 $this->models["TaskCRM"]["id"] = "205";
 $this->models["TaskCRM"]["name"] = "TaskCRM";
-$this->models["TaskCRM"]["label"] = "اعضای پروژه";
+$this->models["TaskCRM"]["label"] = "کار";
 $this->models["TaskCRM"]["primary_key"] = "id";
 $this->models["TaskCRM"]["fields"] = array(
     "id" => array(
@@ -22,22 +22,44 @@ $this->models["TaskCRM"]["fields"] = array(
         "in_table"=>true,
         "type" => array("type" => "text", "size" => 50,"class"=>"col-md-4")
     ),
+    "start_date" => array(
+        "title" => "start_date",
+        "label" => " تاریخ شروع",
+        "sortable" => true,
+        "in_form" => true,
+        "in_table" => true,
+        "type" => array("type" => "date", "size" => 50, "class" => "col-md-4")
+    ),
+    "organizationId" => array(
+        "title" => "organizationId",
+        "label" => "سازمان/شرکت",
+        "sortable" => true,
+        "in_form" => true,
+        "in_table" => true,
+        "type" => array("type" => "select","auto-select"=>true, "select" => ["model"=> $wpdb->prefix . "hesab_model","where" => "type_id=200", "key" => "id", "label" => "title"], "size" => 50, "class" => "col-md-4")
+    ),
     "ProjectCrmId" => array(
         "title" => "ProjectCrmId",
         "label" => "پروژه",
         "sortable" => true,
-        "is_require" => true,
         "in_form" => true,
         "in_table" => true,
-        "type" => array("type" => "select","auto-select"=>true, "select" => ["model"=> $wpdb->prefix . "hesab_model","where" => "type_id=202", "key" => "id", "label" => "title"], "size" => 50, "class" => "col-md-6")
+        "type" => array("type" => "select","auto-select"=>true, "select" => ["model"=> $wpdb->prefix . "hesab_model","where" => "type_id=202", "key" => "id", "label" => "title"], "size" => 50, "class" => "col-md-4")
+    ),
+    "ContactCrmId" => array(
+        "title" => "ContactCrmId",
+        "label" => "مخاطب",
+        "sortable" => true,
+        "in_form" => true,
+        "in_table" => true,
+        "type" => array("type" => "select","auto-select"=>true, "select" => ["model"=> $wpdb->prefix . "hesab_model","where" => "type_id=201", "key" => "id", "label" => "title"], "size" => 50, "class" => "col-md-4")
     ),
     "user_id" => array(
         "title" => "user_id",
         "label" => "کاربر",
         "sortable" => true,
-        "is_require" => true,
         "in_form" => true,
         "in_table" => true,
-        "type" => array("type" => "select","auto-select"=>true, "select" => ["model"=> $wpdb->prefix . "users","where" => " ", "key" => "ID", "label" => "display_name"], "size" => 50, "class" => "col-md-6")
+        "type" => array("type" => "select","auto-select"=>true, "select" => ["model"=> $wpdb->prefix . "users","where" => " ", "key" => "ID", "label" => "display_name"], "size" => 50, "class" => "col-md-4")
     )
 );
